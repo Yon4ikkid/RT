c8bc6db17b0ec32e7f34ba23901abc8d58688dfd
unknown <yon4ikkid@gmail.com>
2022-10-14 14:55:47 +0300

    removed old files

--- RayTracer/IRenderable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class IRenderable
-{
-public:
-	virtual bool Intersect(const Ray& r, int rgb[3]) const = 0;
-};
--- RayTracer/ISurfaceObject.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "IObject.h"
-
-class ISurfaceObject : public IObject
-{
-public:
-	virtual Vector GetNormal(const Vector& p) const = 0;
-};
