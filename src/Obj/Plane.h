b55a122ee8e1e13e368b87914a61d354ce8997c3
Yon4ikkid <yon4ikkid@gmail.com>
2023-03-07 12:26:22 +0200

    Plane class

--- src/Obj/Plane.h
+++ src/Obj/Plane.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "ISurface.h"
+
+namespace Tracer
+{
+    class Plane : public ISurface
+    {
+    private:
+        Vector normal;
+        
+    public:
+        Plane(Vector a, Vector b, Vector c);
+        bool intersect(const Ray& r, float& out) override;
+		Vector get_normal(const Vector& p) const override;
+    }
+}
